d624233e5b362b014ef4d5ef109bea11717bbead
caiosev <kaiosev9@gmail.com>
2022-05-01 17:46:34 -0300

    desk done

--- Junior/fylo-data-storage-component-master/src/Components/PBarComponent.js
+++ Junior/fylo-data-storage-component-master/src/Components/PBarComponent.js
@@ -14,7 +14,7 @@ export default function PBarComponent() {
             <p className='font-bold'>1000 GB</p>
         </div>
 
-        <div className='w-44 h-20 bg-white text-black rounded-xl flex justify-center items-center m-auto'>
+        <div className='md:after:border-solid md:after:border-t-white md:after:border-t-8 md:after:mt-88px md:after:border-x-transparent md:after:border-x-8 md:after:border-b-0 w-44 md:after:content-[""] h-20 md:absolute md:-mt-44  md:right-36 bg-white text-black rounded-xl md:rounded-baloonMD  flex justify-center items-center m-auto md:after:absolute md:after:ml-40'>
             <h1 className='text-3xl font-bold mr-3'>185</h1>
             <p className='text-xs text-gray-500'>GB LEFT</p>
         </div>
--- Junior/fylo-data-storage-component-master/tailwind.config.js
+++ Junior/fylo-data-storage-component-master/tailwind.config.js
@@ -13,8 +13,12 @@ module.exports = {
         'verylightpink': '#FA9E98',
         'lightpink' : '#F95295' 
       },
+      margin :{
+        '88px' :'88px'
+      },
       borderRadius:{
-        'grande':'130px'
+        'grande':'130px',
+        'baloonMD': '0.75rem 0.75rem 0 0.75rem'
       }
     }
   }
