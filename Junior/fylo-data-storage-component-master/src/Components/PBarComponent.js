a982a2a3c0b6f14999f98bda36cb9800a32e2e92
caiosev <kaiosev9@gmail.com>
2022-05-01 12:06:28 -0300

    progress bar done

--- Junior/fylo-data-storage-component-master/src/Components/PBarComponent.js
+++ Junior/fylo-data-storage-component-master/src/Components/PBarComponent.js
@@ -5,8 +5,8 @@ export default function PBarComponent() {
     <div className='mt-5 rounded-xl h-44 w-90% bg-darkblue mx-4 text-white'>
         <p className='text-ts mx-9 mt-9 mb-4'>You've used <span className='font-bold'>815 GB</span> of your storage</p>
         
-        <div class="w-80% mx-8 bg-gray-200 rounded-full">
-             <div className="bg-red-600 text-xs h-4 font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width:'81%'}}></div>
+        <div class="w-80% mx-8 bg-moredarkblue rounded-full">
+             <div className="bg-gradient-to-r from-verylightpink to-lightpink text-xs h-4 font-medium text-blue-100 text-center p-0.5 leading-none rounded-full flex justify-end items-center	" style={{width:'81%'}}><figure class="block bg-white rounded-full h-3 w-3"></figure></div>
         </div>
 
         <div className='flex mx-8 mt-3 place-content-between'>
--- Junior/fylo-data-storage-component-master/tailwind.config.js
+++ Junior/fylo-data-storage-component-master/tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
       },
       colors:{
         'darkblue':'#1E2C69',
-        'moredarkblue':'#0B122E'
+        'moredarkblue':'#0B122E',
+        'verylightpink': '#FA9E98',
+        'lightpink' : '#F95295' 
       },
       borderRadius:{
         'grande':'130px'
